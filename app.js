1f76370658df2137374b6902b653b98e9417f651
AkashSathe <44794849+Akash-1995@users.noreply.github.com>
2022-04-26 22:37:39 +0100

    updates

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//
+let pswd = document.querySelector('#pwd');
+
+let cpswd = document.querySelector('#cpwd');
+
+let msg = document.querySelector('.message');
+let button = document.querySelector('.btn');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  chechkPswd();
+});
+
+function chechkPswd() {
+  const pswdValue = pswd.value;
+  const cpswdValue = cpswd.value;
+
+  if (pswdValue.length != 0) {
+    if (pswdValue == cpswdValue) {
+      msg.style.display = 'none';
+    } else {
+      msg.style.display = 'block';
+    }
+  }
+}
